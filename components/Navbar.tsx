fe35ec0c8746e4a0a139ce59e857740a2e5e60b8
slydragonn <alogo18m@gmail.com>
2022-10-25 18:59:42 -0500

    feat: add login button

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import { CgMenu as MenuIcon } from 'react-icons/cg'
 import { LogoSvg } from './Logo'
 import LinkItem from './LinkItem'
+import Button from './Button'
 
 const Navbar = () => {
   const [showMenu, setShowMenu] = useState<'hidden' | 'visible'>('hidden')
@@ -14,24 +15,27 @@ const Navbar = () => {
   }
 
   return (
-    <nav className="z-30 fixed inset-0 flex justify-center items-center w-full h-16 text-dark font-thin bg-white/50 backdrop-blur-sm px-5">
+    <nav className="z-30 fixed inset-0 flex justify-center items-center w-full h-16 text-dark font-thin bg-white/80 backdrop-blur-sm px-5">
       <div className="flex w-full md:w-[768px] justify-between">
         <LinkItem href="/" isExternal={false}>
-          <LogoSvg />
+          <LogoSvg className='hover:rotate-12 transition-all duration-200' />
         </LinkItem>
-        <ul className="hidden md:flex w-[200px] justify-between text-lg">
-          <li className="hover:underline">
+        <ul className="hidden md:flex w-[200px] justify-between items-center text-lg">
+          <li className="hover:underline font-normal">
             <LinkItem href="/editor" isExternal={false}>
-              editor
+              Editor
             </LinkItem>
           </li>
-          <li className="hover:underline">
-            <LinkItem href="/trunk" isExternal={false}>
-              trunk
+          <li>
+            <LinkItem href="/login" isExternal={false}>
+              <Button size={{w: '100px', h: '40px'}} theme='dark' styles='font-semibold'>Login</Button>
             </LinkItem>
           </li>
         </ul>
-        <section className="relative block md:hidden">
+        <section className="relative flex md:hidden">
+          <LinkItem href="/login" isExternal={false}>
+              <Button size={{w: '100px', h: '40px'}} theme='dark' styles='font-semibold mr-4'>Login</Button>
+          </LinkItem>
           <button
             onClick={toggleMenuVisibility}
             className="hover:-translate-y-1 transition-all duration-300"
@@ -40,16 +44,16 @@ const Navbar = () => {
           </button>
           <ul
             style={{ visibility: showMenu }}
-            className="absolute top-8 right-0 flex flex-col items-center justify-around w-36 h-28 bg-dark text-white text-lg rounded p-5 shadow-lg"
+            className="absolute top-11 right-0 flex flex-col items-center justify-around w-36 h-28 bg-dark text-white text-lg rounded p-5 shadow-lg"
           >
             <li className="hover:underline">
               <LinkItem href="/editor" isExternal={false}>
-                editor
+                Editor
               </LinkItem>
             </li>
             <li className="hover:underline">
               <LinkItem href="/trunk" isExternal={false}>
-                trunk
+                Trunk
               </LinkItem>
             </li>
           </ul>
