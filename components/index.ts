7f8d4d7137609fa110772362d63071ba6b7b978b
slydragonn <alogo18m@gmail.com>
2022-10-25 15:22:50 -0500

    chore: add index page

--- components/index.ts
+++ components/index.ts
@@ -1,1 +1,4 @@
 export { default as Navbar } from './Navbar'
+export { default as Button } from './Button'
+export { default as RandomColorLogo } from './Logo'
+export { default as LinkItem } from './LinkItem'
