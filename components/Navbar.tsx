882018cd344368e642d466569486b0feed95d23a
slydragonn <alogo18m@gmail.com>
2022-10-25 15:21:55 -0500

    feat(components): add Navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,34 +1,60 @@
-import NextLink from 'next/link'
-import { ReactNode } from 'react'
-
-interface LinkItemProps {
-  children: ReactNode
-  href: string,
-  isExternal: boolean
-}
-
-const LinkItem = ({children, href, isExternal}: LinkItemProps) => {
-  if(!isExternal) {
-    return (
-      <NextLink href={href}>
-        <a>{children}</a>
-      </NextLink>
-    )
-  }
-  return (
-    <NextLink href={href} passHref>
-      <a target='_blank'>{children}</a>
-    </NextLink>
-  )
-}
+import { useState } from 'react'
+import { CgMenu as MenuIcon } from 'react-icons/cg'
+import { LogoSvg } from './Logo'
+import LinkItem from './LinkItem'
 
 const Navbar = () => {
+  const [showMenu, setShowMenu] = useState<'hidden' | 'visible'>('hidden')
+
+  const toggleMenuVisibility = () => {
+    if (showMenu === 'hidden') {
+      return setShowMenu(() => 'visible')
+    }
+    return setShowMenu(() => 'hidden')
+  }
+
   return (
-    <nav className='z-30 fixed inset-0 flex justify-around items-center w-full h-14 bg-white/50 backdrop-blur-sm'>
-      <ul className='flex w-[200px] justify-between'>
-        <li><LinkItem href='/editor' isExternal={false}>editor</LinkItem></li>
-        <li><LinkItem href='/trunk' isExternal={false}>trunk</LinkItem></li>
-      </ul>
+    <nav className="z-30 fixed inset-0 flex justify-center items-center w-full h-16 text-dark font-thin bg-white/50 backdrop-blur-sm px-5">
+      <div className="flex w-full md:w-[768px] justify-between">
+        <LinkItem href="/" isExternal={false}>
+          <LogoSvg />
+        </LinkItem>
+        <ul className="hidden md:flex w-[200px] justify-between text-lg">
+          <li className="hover:underline">
+            <LinkItem href="/editor" isExternal={false}>
+              editor
+            </LinkItem>
+          </li>
+          <li className="hover:underline">
+            <LinkItem href="/trunk" isExternal={false}>
+              trunk
+            </LinkItem>
+          </li>
+        </ul>
+        <section className="relative block md:hidden">
+          <button
+            onClick={toggleMenuVisibility}
+            className="hover:-translate-y-1 transition-all duration-300"
+          >
+            <MenuIcon className="w-7 h-7 text-dark" />
+          </button>
+          <ul
+            style={{ visibility: showMenu }}
+            className="absolute top-8 right-0 flex flex-col items-center justify-around w-36 h-28 bg-dark text-white text-lg rounded p-5 shadow-lg"
+          >
+            <li className="hover:underline">
+              <LinkItem href="/editor" isExternal={false}>
+                editor
+              </LinkItem>
+            </li>
+            <li className="hover:underline">
+              <LinkItem href="/trunk" isExternal={false}>
+                trunk
+              </LinkItem>
+            </li>
+          </ul>
+        </section>
+      </div>
     </nav>
   )
 }
